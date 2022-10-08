339a8d154e5d75610bb3d5e10886e07ba5704115
Danielle Sears <daniellejsears@gmail.com>
2022-10-08 12:50:58 -0700

    final commit

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -3,12 +3,12 @@
 -- SELECT sql_schema.employees.empno, firstname, lastname, sex, salary
 -- FROM sql_schema.employees
 -- INNER JOIN sql_schema.salaries 
--- ON sql_schema.employees.empno=sql_schema.salaries.empno
+-- ON sql_schema.employees.empno=sql_schema.salaries.empno;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 -- SELECT firstname, lastname, hiredate 
 -- FROM sql_schema.employees
--- WHERE hiredate between '1986-01-01' and '1986-12-31'
+-- WHERE hiredate between '1986-01-01' and '1986-12-31';
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 -- SELECT sql_schema.dept_managers.deptno, sql_schema.dept_managers.empno, 
@@ -17,7 +17,7 @@
 -- INNER JOIN sql_schema.employees
 -- ON sql_schema.employees.empno=sql_schema.dept_managers.empno
 -- INNER JOIN sql_schema.departments
--- ON sql_schema.departments.deptno=sql_schema.dept_managers.deptno
+-- ON sql_schema.departments.deptno=sql_schema.dept_managers.deptno;
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 -- SELECT sql_schema.dept_emp.empno, 
@@ -26,33 +26,33 @@
 -- INNER JOIN sql_schema.employees
 -- ON sql_schema.employees.empno=sql_schema.dept_emp.empno
 -- INNER JOIN sql_schema.departments
--- ON sql_schema.departments.deptno=sql_schema.dept_emp.deptno
+-- ON sql_schema.departments.deptno=sql_schema.dept_emp.deptno;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 -- SELECT firstname, lastname, sex FROM sql_schema.employees
 -- WHERE firstname = 'Hercules'
--- AND lastname LIKE 'B%'
+-- AND lastname LIKE 'B%';
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 -- SELECT firstname, lastname, deptname, sql_schema.dept_emp.empno
 -- FROM sql_schema.employees
--- INNER JOIN sql_schema.dept_emp
--- ON sql_schema.employees.empno=sql_schema.dept_emp.empno
--- INNER JOIN sql_schema.departments
--- ON sql_schema.employees.empno=sql_schema.departments.deptno
--- WHERE deptname = 'Sales'
+-- INNER JOIN sql_schema.dept_emp ON
+-- sql_schema.employees.empno = sql_schema.dept_emp.empno
+-- INNER JOIN sql_schema.departments ON
+-- sql_schema.dept_emp.deptno = sql_schema.departments.deptno 
+-- WHERE deptname = 'Sales';
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 -- SELECT firstname, lastname, deptname, sql_schema.dept_emp.empno
 -- FROM sql_schema.employees
--- WHERE deptname = 'Sales', 'Development'
 -- INNER JOIN sql_schema.dept_emp
 -- ON sql_schema.employees.empno=sql_schema.dept_emp.empno
--- INNER JOIN sql_schema.departments
--- ON sql_schema.employees.empno=sql_schema.departments.deptno
+-- INNER JOIN sql_schema.departments ON
+-- sql_schema.dept_emp.deptno = sql_schema.departments.deptno 
+-- WHERE deptname IN ('Sales', 'Development')
 
---List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
--- SELECT lastname
+--List the frequency count of employee last names (i.e., howgit many employees share each last name) in descending order.
+-- SELECT lastname, COUNT(lastname) AS Frequency
 -- FROM sql_schema.employees
--- GROUP BY lastname(COUNT)
--- ORDER BY lastname DESC
+-- GROUP BY lastname
+-- ORDER BY COUNT(lastname) DESC;
